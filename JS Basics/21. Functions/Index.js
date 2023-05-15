542287a97bd6c866ae3df0df70a0151410ce12d2
NathDCoder <themusicemperor@gmail.com>
2023-05-15 19:23:36 -0400

    jsbasics

---
+++ JS Basics/21. Functions/Index.js
@@ -0,0 +1,17 @@
+/* A Function defines code once, and uses it many times
+ To perform the code we must call the function */
+
+let userName = "Bro";
+let age = 21;
+
+
+
+happyAnniversary();
+
+function happyAnniversary(){
+    console.log("Happy Anniversary!")
+    console.log("Happy Anniversary!")
+    console.log("Happy Anniversary to __!")
+    console.log("Happy Anniversary!")
+    console.log("Happy ____ Anniversary!")
+}
